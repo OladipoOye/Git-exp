dded1019e99f9c0314356231e4e260f81af48737
OladipoOye <oo312@cam.ac.uk>
2024-01-20 12:05:44 +0000

    Python with testing, anaconda operation

--- # This is my test python file for reposi.py
+++ # This is my test python file for reposi.py
@@ -1,15 +1,21 @@
+#Works
 # This is my test python file for repositories
+import pytest
 print("my life is great")
 def f(x):
     if x < 0:
      raise ValueError("Not a nice val")
     return x**2 + x**0
 print(f(5))
-print(f(-1))
+with pytest.raises(ValueError):
+ f(-1)
 assert f(5) == 26
 assert f(4) == 17
 
 # How branches work 
 print("Branch start")
-#import numpy as np
-#import math
+import numpy as np
+import math
+y = np.sqrt(4)
+print(f(y))
+assert f(y) == f(2)
