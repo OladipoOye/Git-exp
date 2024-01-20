6ee53b64d584639a2091faffa63872d9b3dcb1ad
OladipoOye <oo312@cam.ac.uk>
2024-01-20 10:30:58 +0000

    adding the python file

---
+++ # This is my test python file for reposi.py
@@ -0,0 +1,8 @@
+# This is my test python file for repositories
+print("my life is great")
+def f(x):
+    if x < 0:
+     raise ValueError("Not a nice val")
+    return x**2 + x**0
+print(f(5))
+print(f(-1))
