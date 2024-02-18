18eda5e2a7fd6ff9820093d9f658060806901273
OladipoOye <oo312@cam.ac.uk>
2024-02-18 01:28:09 +0000

    digital pulse attenuation

--- Graph plotting/Fourrier/digital_pulse_att.py
+++ Graph plotting/Fourrier/digital_pulse_att.py
@@ -2,16 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot(n=5, T=np.pi, d=0):
+def plot(n, T, d):
     an_term = []
     bn_term = []
     f = []
-    t = np.linspace(0, 2*np.pi, 50)
-    for i in range(n):
+    t = np.linspace(0, 2*np.pi, n)
+    for i in range(1, n+1):
         an_term.append(np.sin(i*T) / i / np.pi * np.cos(i*t))
         bn_term.append((1 - np.cos(i*T)) / i / np.pi * np.sin(i*t))
-        f.append(an_term[i] + bn_term[i])
-    final = [ j + T/2/np.pi for j in f]
+        f.append(an_term[i-1] + bn_term[i-1])
+    final = [j + d for j in f]
     plt.plot(t, final, 'r', label='fourrier q1')
     plt.xlabel('t')
     plt.ylabel('f(t)')
+    plt.axis([t[0], t[-1], -0.25, 1.25])
+    #  plt.legend()
+    plt.show()
+
+
+plot(n=20, T=np.pi, d=0.5)
