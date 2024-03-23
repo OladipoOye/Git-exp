d26465124a9363513bc649b44339c60eb4e1a631
OladipoOye <oo312@cam.ac.uk>
2024-03-23 21:05:22 +0000

    impulse for second order

--- Graph plotting/Mechanical Vibrations/second_order.py
+++ Graph plotting/Mechanical Vibrations/second_order.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 # w_n = sqrt(k/m)
 def step_underd(N, w_n, zeta, x0):
     w_d = w_n * np.sqrt(1 - (zeta**2))
-    phi = math.atan(zeta * w_d / w_n)
+    phi = np.arctan(zeta * w_d / w_n)
     t_list = np.linspace(0, 5, N-1)
     y_list = []
     approx_list = []
@@ -35,12 +35,35 @@ def step_underd(N, w_n, zeta, x0):
     plt.plot(t_list, approx_list, 'm*', label='small angle approx')
     plt.xlabel('time')
     plt.ylabel('displacement')
+    plt.title('Second order step response')
     plt.legend()
     plt.show()
     
-step_underd(100, 3, 0.3, 20)
+#step_underd(100, 3, 0.3, 20)
 # Only zeta affects the small angle approximation
 # changing of w_n doesn't affect approximation
 # increasing w_n makes the initial gradient steeper
 # all graphs start as sinusodal, then level off
 # changing x0 only changes the magnitude of the displacement, not shape
+
+def imp_underd(N, w_n, zeta, I):
+    w_d = w_n * np.sqrt(1 - (zeta**2))
+    phi = np.arctan(zeta * w_n/w_d)
+    t_list = np.linspace(0, 10, N-1)
+    y_list = []
+    def y_imp(t):
+        y = I / np.cos(phi) * ((-zeta*w_n*np.exp(-zeta*w_n*t)*np.cos(w_d*t - phi)) + (w_d*np.exp(-zeta*w_n*t)*np.sin(w_d*t - phi)))
+        return y 
+    for i in range(N-1):
+        y_list.append(y_imp(t_list[i]))
+    plt.plot(t_list, y_list, 'rx')
+    plt.xlabel('time')
+    plt.ylabel('displacement')
+    plt.title('Second Order Impulse response')
+    plt.show()
+
+imp_underd(100, 3, 0.3, 50)
+# oscillates between negative and positive
+# increasing the natural time period squishes the graph
+# decreasing zeta reduces the damping
+# changing the impulse only affects the magnitude of the displacement, not the shape
